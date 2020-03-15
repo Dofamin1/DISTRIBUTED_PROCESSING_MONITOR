419defe089420658e29b6dd358fa91123d08fe47
Anton Kovalenko <gugenot990@gmail.com>
2020-03-15 20:14:06 +0200

    add viz.js library to render graphs

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,33 @@
   <v-app>
       <div id="app">
         <img alt="Vue logo" id="logo" src="./assets/logo.png">
+        <div v-html="graphHTML"></div>
       </div>
   </v-app>
 </template>
 
 <script>
 import SocketClient from './socketClient'
+import viz from './vizRenderer'
 export default {
   name: 'App',
   created() {
     this.socketClient.onEvent('nodes_graph', this.onGraphChange);
-    this.socketClient.onEvent('queue_length', this.onQueueLengthChange)
+    this.socketClient.onEvent('queue_length', this.onQueueLengthChange);
   },
   data: () => {
     return {
-      socketClient: new SocketClient()
+      socketClient: new SocketClient(),
+      viz,
+      graphHTML: null
     }
   },
   methods: {
     onGraphChange(graph) {
       console.log(graph);
-      throw new Error("onGraphChange is not implemented")
+      this.viz.renderString('digraph { a -> b }') //TODO:replace with graph
+        .then(result => this.graphHTML = result)
+        .catch(err => console.log(err));
     },
     onQueueLengthChange(length) {
       console.log(length);
---
+++ src/vizRenderer.js
@@ -0,0 +1,4 @@
+import Viz from 'viz.js';
+const { Module, render } = require('viz.js/full.render.js');
+
+export default new Viz({ Module, render });
